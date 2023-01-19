08abe62b4d62eb27d67f2c0aff5bccdbdeb1c306
MINGYU <brianmg@naver.com>
2023-01-19 11:41:39 +0900

    .

--- src/ch2/thisex/BirthDay.java
+++ src/ch2/thisex/BirthDay.java
@@ -5,6 +5,25 @@ public class BirthDay {
     int month;
     int year;
 
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
     public void setYear(int year) {
         this.year = year;
     }
@@ -12,11 +31,24 @@ public class BirthDay {
         System.out.println(this);
     }
 
+    @Override
+    public String toString() {
+        return "BirthDay{" +
+                "day=" + day +
+                ", month=" + month +
+                ", year=" + year +
+                '}';
+    }
+
     public class ThisExample{
         public static void main(String []args){
             BirthDay bday = new BirthDay();
+            bday.setDay(22);
+            bday.setMonth(01);
             bday.setYear(2023);
             bday.printThis();
+
+            System.out.println(bday.toString());
 
         }
 
